66867559d1db5b5053ca0fdfc870e0d1591d17ea
James Pittard <jp17358@bristol.ac.uk>
2022-04-21 12:16:26 +0100

    Added commandline arguments with error management.

--- steinhardt_freeze.py
+++ steinhardt_freeze.py
@@ -1,5 +1,7 @@
 
 
+from ast import expr_context
+from multiprocessing.sharedctypes import Value
 import steinhardt
 import tools
 import data_file_maker
@@ -7,7 +9,7 @@ import data_file_maker
 import os
 import sys
 import matplotlib.pyplot as plt
-
+import pprint
 
 class Steinhardt_Frame:
 
@@ -25,15 +27,16 @@ class Steinhardt_Frame:
         os.chdir(current_dir)
 
 
-def main(file, steinhardt_settings_dict):
+def main(steinhardt_settings_dict):
+
+    ##### Creating results directory and reading jmol_all.xyz #####
 
     current_dir = os.path.dirname(os.path.realpath(__file__))
 
-    file = 'results/' + file
+    file = 'results/' + steinhardt_settings_dict['file']
  
     path = f"{current_dir}/{file}/steinhardt_results"
     tools.mkdir(path)
-
 
     all_xyz = tools.file_proc(f"{current_dir}/{file}/jmol_all.xyz", seperator= "\n\n")
     xyz_files = [file.split("\n") for file in all_xyz] 
@@ -41,10 +44,22 @@ def main(file, steinhardt_settings_dict):
 
     times = [float(xyz_file[1].split(' ')[-1]) for xyz_file in xyz_files]
 
-    timestep_indexes = [i for i in range(1, len(times), int(len(times)/4))]
+    ##### Fetching data from required timesteps ####
 
-    times_to_plot = [times[i] for i in timestep_indexes]
+    if len(steinhardt_settings_dict['timesteps']) == 0:
+        frames = steinhardt_settings_dict['frames']
+        timestep_indexes = [i for i in range(1, len(times), int(len(times)/frames))]
+        times_to_plot = [times[i] for i in timestep_indexes]
+
+    elif len(steinhardt_settings_dict['timesteps']) != 0:
+        times_to_plot = [tools.closest_to(time,times) for time in steinhardt_settings_dict['timesteps']]
+        timestep_indexes = [times.index(time) for time in times_to_plot]
+
+
     xyz_files_to_plot = [all_xyz[i] for i in timestep_indexes]
+
+
+    ##### Going through frames and generating paramters #####
 
     frames = [Steinhardt_Frame(xyz_file) for xyz_file in xyz_files_to_plot]
 
@@ -67,11 +82,11 @@ def main(file, steinhardt_settings_dict):
 
     #avg_str, avg_dict, full_array = steinhardt.averages(file_arrs, center_only= True, zlims = [-20,0]) 
     #avg_str, avg_dict, full_array = steinhardt.averages(file_arrs, center_only= True) 
-    avg_str, avg_dict, full_array = steinhardt.averages(file_arrs)
+    avg_str, avg_dict, full_array = steinhardt.averages(file_arrs, centre_only = steinhardt_settings_dict['reference_gen'])
 
     steinhardt.histogram(full_array, path)
 
-    array_str = 'z, Q4, Q6, Q8, Q10, Q12\n\n'
+    array_str = 'z, Q4, Q6, Q8, Q10, Q12\n\n' #read from settings.csv
     for line in full_array:
         array_str += "\n" + str(line)
 
@@ -106,27 +121,49 @@ if __name__ == '__main__':
                                     frames = 4,
                                     timesteps = [],
                                     line_plots = True,
-                                    histograms = False)
+                                    histograms = False,
+                                    reference_gen = False,
+                                    zlims = [10,25])
 
 
     try:
-        file = sys.argv[1]
+        steinhardt_settings_dict['file'] = sys.argv[1]
+
+        for arg in sys.argv[2:]:
+            
+            arg = arg.split('-')
+
+            if len(arg) == 1:
+                print("\nERROR: Input arguments as: frames-4 etc.\n")
+                break
+
+            try:
+                steinhardt_settings_dict[arg[0]] = tools.str_to_bool(arg[1])
+            except TypeError:
+                try:
+                    steinhardt_settings_dict[arg[0]] = tools.str_to_float(arg[1])
+                except TypeError or ValueError:
+                    try:
+                        steinhardt_settings_dict[arg[0]] = tools.str_to_list(arg[1])
+                    except TypeError:
+                        steinhardt_settings_dict[arg[0]] = arg[1]
+                    
+                    
+            if len(steinhardt_settings_dict['timesteps']) != 0:
+                steinhardt_settings_dict['frames'] = len(steinhardt_settings_dict['timesteps'])
+
+
+        pprint.pprint(steinhardt_settings_dict)
+
+        main(steinhardt_settings_dict)
+
     except IndexError:
-        print("ERROR: No Filename Provided.")
+        print("\nERROR: No Filename Provided.")
+        print("\nDefault arguments: ")
+        pprint.pprint(steinhardt_settings_dict)
+        print("\nzlims only applied if reference_gen is True.\n")
+        
 
-    for arg in sys.argv:
-        arg = arg.split('-')
-
-        if arg[0] == 'frames':
-            steinhardt_settings_dict['frames'] = float(arg[1])
-
-        if arg[0] == 'timesteps':
-            steinhardt_settings_dict['frames'] = float(arg[1])
-
-        steinhardt_settings_dict[arg[0]] = arg[1]
-
-
-    main(file)
 
 
 ####TODO######
